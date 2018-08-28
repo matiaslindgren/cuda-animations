5978c2f336a785c5196092c81711e9b8e6a960e5
Matias Lindgren <matias.lindgren@gmail.com>
2018-08-28 18:12:36 +0300

    Add actual CUDA kernels from PPC 2018

--- cuda/main.cu
+++ cuda/main.cu
@@ -1,7 +1,9 @@
+// kernels from http://ppc.cs.aalto.fi/ch4 (2018)
 #include <algorithm>
 #include <iostream>
 #include <limits>
 #include <random>
+#include <chrono>
 
 #include <cstdio>
 #include <cuda_runtime.h>
@@ -25,7 +27,15 @@ float next_float() {
     return floats(e);
 }
 
-__global__ void kernel(const float *in, float *out, int n) {
+inline int static divup(int a, int b) {
+    return (a + b - 1)/b;
+}
+
+inline int static roundup(int a, int b) {
+    return divup(a, b) * b;
+}
+
+__global__ void kernel_v0(const float *in, float *out, int n) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     float v = HUGE_VALF;
@@ -38,30 +48,182 @@ __global__ void kernel(const float *in, float *out, int n) {
     out[n*i + j] = v;
 }
 
+__global__ void kernel_v1(const float *in, float *out, int n) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    float v = HUGE_VALF;
+    for (int k = 0; k < n; ++k) {
+        float x = in[n*j + k];
+        float y = in[n*k + i];
+        float z = x + y;
+        v = min(v, z);
+    }
+    out[n*j + i] = v;
+}
+
+__global__ void kernel_v2(float* r, const float* d, int n, int nn) {
+    int ia = threadIdx.x;
+    int ja = threadIdx.y;
+    int ic = blockIdx.x;
+    int jc = blockIdx.y;
+
+    const float* t = d + nn * nn;
+
+    float v[8][8];
+    for (int ib = 0; ib < 8; ++ib) {
+        for (int jb = 0; jb < 8; ++jb) {
+            v[ib][jb] = HUGE_VALF;
+        }
+    }
+    for (int k = 0; k < n; ++k) {
+        float x[8];
+        float y[8];
+        for (int ib = 0; ib < 8; ++ib) {
+            int i = ic * 64 + ib * 8 + ia;
+            x[ib] = t[nn*k + i];
+        }
+        for (int jb = 0; jb < 8; ++jb) {
+            int j = jc * 64 + jb * 8 + ja;
+            y[jb] = d[nn*k + j];
+        }
+        for (int ib = 0; ib < 8; ++ib) {
+            for (int jb = 0; jb < 8; ++jb) {
+                v[ib][jb] = min(v[ib][jb], x[ib] + y[jb]);
+            }
+        }
+    }
+    for (int ib = 0; ib < 8; ++ib) {
+        for (int jb = 0; jb < 8; ++jb) {
+            int i = ic * 64 + ib * 8 + ia;
+            int j = jc * 64 + jb * 8 + ja;
+            if (i < n && j < n) {
+                r[n*i + j] = v[ib][jb];
+            }
+        }
+    }
+}
+
+__global__ void add_padding_v2(const float* r, float* d, int n, int nn) {
+    int ja = threadIdx.x;
+    int i = blockIdx.y;
+
+    float* t = d + nn * nn;
+
+    for (int jb = 0; jb < nn; jb += 64) {
+        int j = jb + ja;
+        float v = (i < n && j < n) ? r[n*i + j] : HUGE_VALF;
+        d[nn*i + j] = v;
+        t[nn*j + i] = v;
+    }
+}
+
+void step_v0(float* r, const float* d, int n) {
+    // Allocate memory & copy data to GPU
+    float* dGPU = NULL;
+    CHECK(cudaMalloc((void**)&dGPU, n * n * sizeof(float)));
+    float* rGPU = NULL;
+    CHECK(cudaMalloc((void**)&rGPU, n * n * sizeof(float)));
+    CHECK(cudaMemcpy(dGPU, d, n * n * sizeof(float), cudaMemcpyHostToDevice));
+
+    // Run kernel
+    dim3 dimBlock(16, 16);
+    dim3 dimGrid(divup(n, dimBlock.x), divup(n, dimBlock.y));
+    kernel_v0<<<dimGrid, dimBlock>>>(rGPU, dGPU, n);
+    CHECK(cudaGetLastError());
+
+    // Copy data back to CPU & release memory
+    CHECK(cudaMemcpy(r, rGPU, n * n * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(dGPU));
+    CHECK(cudaFree(rGPU));
+}
+
+void step_v1(float* r, const float* d, int n) {
+    // Allocate memory & copy data to GPU
+    float* dGPU = NULL;
+    CHECK(cudaMalloc((void**)&dGPU, n * n * sizeof(float)));
+    float* rGPU = NULL;
+    CHECK(cudaMalloc((void**)&rGPU, n * n * sizeof(float)));
+    CHECK(cudaMemcpy(dGPU, d, n * n * sizeof(float), cudaMemcpyHostToDevice));
+
+    // Run kernel
+    dim3 dimBlock(16, 16);
+    dim3 dimGrid(divup(n, dimBlock.x), divup(n, dimBlock.y));
+    kernel_v1<<<dimGrid, dimBlock>>>(rGPU, dGPU, n);
+    CHECK(cudaGetLastError());
+
+    // Copy data back to CPU & release memory
+    CHECK(cudaMemcpy(r, rGPU, n * n * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(dGPU));
+    CHECK(cudaFree(rGPU));
+}
+
+void step_v2(float* r, const float* d, int n) {
+    int nn = roundup(n, 64);
+
+    // Allocate memory & copy data to GPU
+    float* dGPU = NULL;
+    CHECK(cudaMalloc((void**)&dGPU, 2 * nn * nn * sizeof(float)));
+    float* rGPU = NULL;
+    CHECK(cudaMalloc((void**)&rGPU, n * n * sizeof(float)));
+    CHECK(cudaMemcpy(rGPU, d, n * n * sizeof(float), cudaMemcpyHostToDevice));
+
+    // Run kernel
+    {
+        dim3 dimBlock(64, 1);
+        dim3 dimGrid(1, nn);
+        add_padding_v2<<<dimGrid, dimBlock>>>(rGPU, dGPU, n, nn);
+        CHECK(cudaGetLastError());
+    }
+
+    // Run kernel
+    {
+        dim3 dimBlock(8, 8);
+        dim3 dimGrid(nn / 64, nn / 64);
+        kernel_v2<<<dimGrid, dimBlock>>>(rGPU, dGPU, n, nn);
+        CHECK(cudaGetLastError());
+    }
+
+    // Copy data back to CPU & release memory
+    CHECK(cudaMemcpy(r, rGPU, n * n * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(dGPU));
+    CHECK(cudaFree(rGPU));
+}
 
 int main() {
     const size_t n = BLOCKSIZE << 7;
     const size_t data_size = n * n * sizeof(float);
 
-    // Fill vector with random floats from uniform(0, 1)
-    std::vector<float> data(n*n);
-    std::generate(data.begin(), data.end(), next_float);
+    {
+        const auto time_start = std::chrono::high_resolution_clock::now();
+        std::vector<float> data(n*n);
+        std::generate(data.begin(), data.end(), next_float);
+        std::vector<float> result(n*n);
+        step_v0(result, data, n);
+        const auto time_end = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<float> delta_seconds = time_end - time_start;
+        std::cout << std::setprecision(7) << delta_seconds.count() << std::endl;
+    }
 
-    float* device_data = NULL;
-    CHECK(cudaMalloc(&device_data, data_size));
-    CHECK(cudaMemcpy(device_data, data.data(), data_size, cudaMemcpyHostToDevice));
+    {
+        const auto time_start = std::chrono::high_resolution_clock::now();
+        std::vector<float> data(n*n);
+        std::generate(data.begin(), data.end(), next_float);
+        std::vector<float> result(n*n);
+        step_v1(result, data, n);
+        const auto time_end = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<float> delta_seconds = time_end - time_start;
+        std::cout << std::setprecision(7) << delta_seconds.count() << std::endl;
+    }
 
-    float* device_result = NULL;
-    CHECK(cudaMalloc(&device_result, data_size));
-
-    dim3 dimGrid(n/BLOCKSIZE, n/BLOCKSIZE);
-    dim3 dimBlock(BLOCKSIZE, BLOCKSIZE);
-
-    kernel<<<dimGrid, dimBlock>>>(device_data, device_result, n);
-    CHECK(cudaGetLastError());
-
-    CHECK(cudaMemcpy(data.data(), device_result, data_size, cudaMemcpyDeviceToHost));
-    CHECK(cudaFree(device_data));
-    CHECK(cudaFree(device_result));
+    {
+        const auto time_start = std::chrono::high_resolution_clock::now();
+        std::vector<float> data(n*n);
+        std::generate(data.begin(), data.end(), next_float);
+        std::vector<float> result(n*n);
+        step_v2(result, data, n);
+        const auto time_end = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<float> delta_seconds = time_end - time_start;
+        std::cout << std::setprecision(7) << delta_seconds.count() << std::endl;
+    }
 
 }
