cf058cc2ce0d50dac1693e538870ed7d91276ea8
Matias Lindgren <matias.lindgren@gmail.com>
2019-05-01 16:13:41 +0300

    Add device id parameter for analyzer

--- cuda/analyze.cu
+++ cuda/analyze.cu
@@ -1,7 +1,12 @@
 #include "device_analysis.hpp"
+#include <cstdlib>
 
-int main() {
-    const DeviceAnalyzer analyzer(0);
-    std::cout << "GPU stats:" << std::endl;
-    analyzer.print_stats();
+int main(int argc, char** argv) {
+	int device_num = 0;
+	if (argc == 2) {
+		device_num = std::stoi(argv[1]);
+	}
+	const DeviceAnalyzer analyzer(device_num);
+	std::cout << "Device " << device_num << " stats:\n";
+	analyzer.print_stats();
 }
